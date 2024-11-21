7c3b4c08463671728848de1d51dbaeb289a8d0dc
Gavin Riggi <gavin.riggi@wellspring.com>
2024-11-21 17:23:00 -0500

    fix: access parsed json after we parse the json perhaps

--- lib/puppet_x/griggi/azurekv/lookup.rb
+++ lib/puppet_x/griggi/azurekv/lookup.rb
@@ -138,8 +138,8 @@ module PuppetX
             raise Puppet::Error, "[AZUREKV]: Non-specific error when looking up #{id}: #{response.body}"
           end
 
-          Puppet.debug "[AZUREKV]: Response received: #{response.body}."
-          secret = JSON.parse(response.body['value'])
+          Puppet.debug '[AZUREKV]: Response received.'
+          secret = JSON.parse(response.body)['value']
           Puppet.debug '[AZUREKV]: Returning secret as sensitive.'
           Puppet::Pops::Types::PSensitiveType::Sensitive.new(secret)
         end
